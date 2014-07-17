9a450e263be81bef1b92a1806d54422fd07a407a
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2014-07-17 14:19:14 -0500

    update config

---
+++ compile.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -x
+
+
+#echo "####### go fmt AWE     #######"
+go fmt github.com/MG-RAST/AWE/...
+
+echo "####### go fix AWE     #######"
+go fix github.com/MG-RAST/AWE/...
+
+echo "####### go install AWE #######"
+go install -v github.com/MG-RAST/AWE/...
