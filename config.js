b4b0e54edb8d6c3e4e38b0fff996564397141783
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2014-07-30 16:46:53 -0500

    add

---
+++ config.js
@@ -0,0 +1,9 @@
+var RetinaConfig = {
+    "awe_ip": "http://140.221.67.184:8000",
+    "workflow_ip": "http://140.221.67.184:8000/awf",
+    "authResources": { "default": "KBase",
+                       "KBase": { "icon": "KBase_favicon.ico",
+                                  "prefix": "kbgo4711" } },
+    "mgrast_api": "http://api.metagenomics.anl.gov",
+    "authentication": true
+}
