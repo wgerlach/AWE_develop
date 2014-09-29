6b9566c9e0e9571257144c2e6826fbb51b24b89e
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2014-09-29 15:48:09 -0500

    some fixes

--- deploy_awe_client.sh
+++ deploy_awe_client.sh
@@ -7,12 +7,22 @@ export SERVERURL=http://10.1.12.14:8001
 
 export DOCKERBIN=docker
 #export DOCKERBIN=docker.io
-export AWE-BINARY="awe-client-20140926-dd4147757b"
+export AWE_BINARY="awe-client-20140926-dd4147757b"
 
 
 set -x
 
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+	echo "Not running as root"
+	exit
+fi
 
+
+
+if [ $1a == "stopa" ]
+then
+	killall -s TERM awe-client
+fi
 
 if [ $1a == "nodockera" ]
 then
@@ -21,30 +31,30 @@ then
 
 	if ! [ -f /home/ubuntu/awe-client ]; then
 		cd /home/ubuntu/
-		curl -O http://dunkirk.mcs.anl.gov/~wgerlach/${AWE-BINARY} > awe.tmp
+		curl http://dunkirk.mcs.anl.gov/~wgerlach/${AWE_BINARY} > awe.tmp
 		chmod +x awe.tmp
 		mv awe.tmp awe-client
 	fi
 
 else
 
-	sudo start $DOCKERBIN ; echo starting $DOCKERBIN
+	start $DOCKERBIN ; echo starting $DOCKERBIN
 
 	sleep 5
 
 	if ! [ -f awe.tgz ]; then
 	  curl http://shock.metagenomics.anl.gov/node/a8560eb3-d1e7-4fc7-b01e-c7c8a2a544e0?download > awe.tgz
 	fi
-	sudo docker load -i awe.tgz
+	docker load -i awe.tgz
 
-	sudo docker rm -f awe-worker ; echo removing old container
+	docker rm -f awe-worker ; echo removing old container
 
 fi
 
 set -e
 
-sudo rm -rf /mnt/data/awe/logs
-sudo rm -rf /mnt/data/awe/work/*
+rm -rf /mnt/data/awe/logs
+rm -rf /mnt/data/awe/work/*
 mkdir -p /mnt/data/awe/logs
 mkdir -p /home/ubuntu/awe-config
 
@@ -58,13 +68,14 @@ then
 nohup /home/ubuntu/awe-client -debug 2 \
  -server_url=${SERVERURL} \
  -client_group=nodocker \
- -conf /awe-config/awe-client.cfg \
- -cgroup_memory_docker_dir=/cgroup_memory_docker/ \
+ -conf /home/ubuntu/awe-config/awe-client.cfg \
 2> /mnt/data/awe/logs/stderr.log 1> /mnt/data/awe/logs/stdout.log &
 
 else
 
-sudo $DOCKERBIN run -d -t -i --name awe-worker \
+
+# something like: docker cp /home/ubuntu/awe-config/awe-client.cfg awe-worker:/awe-config/
+$DOCKERBIN run -d -t -i --name awe-worker \
  -v /var/run/docker.sock:/var/run/docker.sock \
  -v /home/ubuntu/awe-config/:/awe-config/ \
  -v /sys/fs/cgroup/memory/docker/:/cgroup_memory_docker/ \
